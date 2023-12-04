c462a2145a6434d4a039f3e690139faac5149dea
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-12-04 16:32:33 -0300

    feat: tela de ediçao de treino em Att

--- src/components/Treino/Treino.jsx
+++ src/components/Treino/Treino.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import styles from './Treino.module.css'
 import axios from 'axios'
 import useDeleteTreino from '../../hooks/useDeleteTreino'
-
+import PopEdit from '../PopEdit/PopEdit'
 
 const Treino = ({data, exercicios})=>{
 
     const [textButton, setTextButton] = useState('x')
     const handleDelete = useDeleteTreino()
+    const [editPop, setEditPop] = useState(false)
 
     const deletar = async(id)=>{
         handleDelete(id)
         console.log('deletado')
+    }
+
+    const editar = async()=>{
+        setEditPop(!editPop)
     }
 
 
@@ -28,6 +33,9 @@ const Treino = ({data, exercicios})=>{
 
     return(
         <div className={styles.container_treino}>
+            {editPop && (
+                <PopEdit data={data} fecharTela={editar}/>
+            )}
             <div className={styles.container_exercicioo}>
                 <div className={styles.container_data_treino}>
                     <span>{data.data}</span>
@@ -45,7 +53,10 @@ const Treino = ({data, exercicios})=>{
                 </div>
             </div>
             
-            <button onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={()=> {deletar(data.id)}} className={styles.botaoDeletar}>{textButton}</button>
+           <div className={styles.container_btn_op}>
+                <button onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={()=> {deletar(data.id)}} className={styles.botaoDeletar}>{textButton}</button>
+                <button onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={()=> {editar()}} className={styles.botaoEditar}>Editar</button>
+           </div>
 
         </div>
     )
