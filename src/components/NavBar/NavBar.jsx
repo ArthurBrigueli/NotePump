617f5f35f0244497a733e154fe1a642dde8985a9
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-12-07 05:19:19 -0300

    Feat: Responsividade

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,44 @@
 import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom';
 import logoNav from '../../img/logoNav.jpg'
+import { useState } from 'react';
 
 
 
 const NavBar = ()=>{
+
+
+    const [menuToggle, setMenuToggle] = useState(false)
+
+
+    const handleToggle = ()=>{
+        setMenuToggle(!menuToggle)
+    }
+
     return(
         <nav className={styles.navbar}>
             <div className={styles.container_logo}>
                 <img src={logoNav} alt=""/>
             </div>
-            <ul>
-                <li>
-                    <Link to="/NotePump">NotePump</Link>
-                </li>
-                <li>
-                    <Link to="/MinhaLista">Minha Lista</Link>
-                </li>
-                <li>
-                    <Link to="/NotePump">NotePump</Link>
-                </li>
-            </ul>
-            <div className={styles.container_profile}>
-                <img src={logoNav} alt=""/>
-                <span>Arthur</span>
+            <div className={`${styles.container_nav} ${menuToggle && styles.OpenMenu}`}>
+                <ul>
+                    <li>
+                        <Link to="/NotePump">NotePump</Link>
+                    </li>
+                    <li>
+                        <Link to="/MinhaLista">Minha Lista</Link>
+                    </li>
+                    <li>
+                        <Link to="/NotePump">NotePump</Link>
+                    </li>
+                </ul>
+                <div className={styles.container_profile}>
+                    <img src={logoNav} alt=""/>
+                    <span>Arthur</span>
+                </div>
+            </div>
+            <div className={styles.menuToggle} onClick={handleToggle}>
+                &#9776;
             </div>
         </nav>
     )
