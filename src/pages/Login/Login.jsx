6591269f95f83d7e2b00857451cfde7418ee90ee
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-12-15 12:18:30 -0300

    Update: Estilo da rota login adicionado

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import styles from './Login.module.css'
-
+import imgLogin from '../../img/login-form-img.png'
 
 
 const Login = ()=>{
@@ -7,12 +7,17 @@ const Login = ()=>{
         <section className={styles.section}>
             <div className={styles.container}>
                 <div className={styles.containerInput}>
+                    <h2 className={styles.h2}>Login</h2>
                     <input type="text" name="usuario" id="usuario" placeholder='Usuario'/>
                     <input type="password" name="senha" id="senha" placeholder='Senha'/>
                 </div>
                 <div className={styles.containerBtn}>
-                    <input type="submit" value="Login" />
+                    <input type="submit" value="Login"/>
                 </div>
+            </div>
+
+            <div className={styles.container_img}>
+                <img src={imgLogin} alt="" />
             </div>
         </section>
     )
