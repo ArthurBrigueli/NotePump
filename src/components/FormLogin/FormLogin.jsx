b965ec5a5a84e7f357ef731413318fa56f455d96
Arthur Brigueli <arthurbriguelifort@gmail.com>
2023-12-15 23:48:14 -0300

    feat: Remoção do form dentro da page e transformando em Componente

---
+++ src/components/FormLogin/FormLogin.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import styles from './formLogin.module.css'
+
+const FormLogin = ()=>{
+
+    const [usuario, setUsuario] = useState('')
+    const [senha, setSenha] = useState('')
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(usuario,senha)
+    }
+
+    return(
+        <form onSubmit={handleSubmit} method='POST'  className={styles.container}>
+            <div className={styles.containerInput}>
+                <h2 className={styles.h2}>Login</h2>
+                <input type="text" name="usuario" id="usuario" placeholder='Usuario' onChange={(e)=>{setUsuario(e.target.value)}}/>
+                <input type="password" name="senha" id="senha" placeholder='Senha' onChange={(e)=>{setSenha(e.target.value)}}/>
+            </div>
+            <div className={styles.containerBtn}>
+                <input type="submit" value="Login"/>
+            </div>
+        </form>
+    )
+}
+
+
+export default FormLogin
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,12 @@
 import styles from './Login.module.css'
 import imgLogin from '../../img/login-form-img.png'
+import FormLogin from '../../components/formLogin/FormLogin'
 
 
 const Login = ()=>{
     return(
         <section className={styles.section}>
-            <div className={styles.container}>
-                <div className={styles.containerInput}>
-                    <h2 className={styles.h2}>Login</h2>
-                    <input type="text" name="usuario" id="usuario" placeholder='Usuario'/>
-                    <input type="password" name="senha" id="senha" placeholder='Senha'/>
-                </div>
-                <div className={styles.containerBtn}>
-                    <input type="submit" value="Login"/>
-                </div>
-            </div>
-
+            <FormLogin/>
             <div className={styles.container_img}>
                 <img src={imgLogin} alt="" />
             </div>
